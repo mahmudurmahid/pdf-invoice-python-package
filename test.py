6b90c56a285ac11c613befe1dc6c0046393d796e
mahmudurmahid <m.mahid@icloud.com>
2025-05-08 12:04:36 +0100

    debug code to produce all pdfs in invoices folder, not just one pdf

---
+++ test.py
@@ -0,0 +1,3 @@
+import pdf_invoice_generator
+
+pdf_invoice_generator.generate_invoice_pdf("invoices", "output", "pythonhow.png", "product_id", "product_name", "amount_purchased", "price_per_unit", "total_price")
